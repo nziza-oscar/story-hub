213b0b827c7a48d09b4cb59cb1fb8dcec8a8e48f
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2025-12-07 19:46:12 +0200

    feat: server/routes/notifications.js

--- server/routes/notifications.js
+++ server/routes/notifications.js
@@ -5,7 +5,6 @@ const auth = require("../middleware/authenticate")
 
 router.get('/', auth, NotificationController.getUserNotifications);
 
-// Mark one notification as read
 router.patch('/:id/read',auth, NotificationController.markAsRead);
 router.patch('/user/read-all',auth, NotificationController.markAllAsRead);
 router.delete('/:id',auth, NotificationController.deleteNotification);
