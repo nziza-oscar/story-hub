0a61bd66a1f9e3d74ed169ec100228bba11cc7b6
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2025-12-07 19:47:41 +0200

    dummy user data

---
+++ client/src/utils/data.js
@@ -0,0 +1,34 @@
+export const dumyusers = {
+  followers: [
+    { id: 1, username: "alice", avatar: null, followersCount: 123, followingCount: 45, isFollowing: true },
+    { id: 2, username: "bob", avatar: null, followersCount: 56, followingCount: 12, isFollowing: false },
+    { id: 3, username: "charlie", avatar: null, followersCount: 78, followingCount: 34, isFollowing: true },
+    { id: 4, username: "david", avatar: null, followersCount: 45, followingCount: 23, isFollowing: false },
+    { id: 5, username: "eve", avatar: null, followersCount: 89, followingCount: 50, isFollowing: true },
+    { id: 6, username: "frank", avatar: null, followersCount: 67, followingCount: 28, isFollowing: false },
+    { id: 7, username: "grace", avatar: null, followersCount: 34, followingCount: 19, isFollowing: true },
+    { id: 8, username: "heidi", avatar: null, followersCount: 90, followingCount: 60, isFollowing: false },
+    { id: 9, username: "ivan", avatar: null, followersCount: 55, followingCount: 32, isFollowing: true },
+    { id: 10, username: "judy", avatar: null, followersCount: 43, followingCount: 21, isFollowing: false },
+    { id: 11, username: "karen", avatar: null, followersCount: 77, followingCount: 44, isFollowing: true },
+    { id: 12, username: "leo", avatar: null, followersCount: 61, followingCount: 33, isFollowing: false },
+    { id: 13, username: "mallory", avatar: null, followersCount: 88, followingCount: 55, isFollowing: true },
+    { id: 14, username: "nina", avatar: null, followersCount: 39, followingCount: 20, isFollowing: false }
+  ],
+  following: [
+    { id: 15, username: "oliver", avatar: null, followersCount: 120, followingCount: 60, isFollowing: true },
+    { id: 16, username: "peter", avatar: null, followersCount: 80, followingCount: 40, isFollowing: true },
+    { id: 17, username: "quinn", avatar: null, followersCount: 55, followingCount: 25, isFollowing: true },
+    { id: 18, username: "rachel", avatar: null, followersCount: 70, followingCount: 35, isFollowing: true },
+    { id: 19, username: "steve", avatar: null, followersCount: 60, followingCount: 30, isFollowing: true },
+    { id: 20, username: "trudy", avatar: null, followersCount: 45, followingCount: 22, isFollowing: true },
+    { id: 21, username: "ursula", avatar: null, followersCount: 90, followingCount: 45, isFollowing: true },
+    { id: 22, username: "victor", avatar: null, followersCount: 33, followingCount: 18, isFollowing: true },
+    { id: 23, username: "wendy", avatar: null, followersCount: 50, followingCount: 25, isFollowing: true },
+    { id: 24, username: "xander", avatar: null, followersCount: 40, followingCount: 20, isFollowing: true },
+    { id: 25, username: "yvonne", avatar: null, followersCount: 75, followingCount: 38, isFollowing: true },
+    { id: 26, username: "zack", avatar: null, followersCount: 65, followingCount: 33, isFollowing: true },
+    { id: 27, username: "amy", avatar: null, followersCount: 55, followingCount: 28, isFollowing: true },
+    { id: 28, username: "brad", avatar: null, followersCount: 44, followingCount: 22, isFollowing: true }
+  ]
+};
