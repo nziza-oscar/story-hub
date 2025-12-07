578e8d42999009958d5269004d453a9d6f9063b3
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2025-12-07 19:46:04 +0200

    feat: client/src/store/useUserStore.js

--- client/src/store/useUserStore.js
+++ client/src/store/useUserStore.js
@@ -1,18 +1,32 @@
 import { create} from "zustand"
 import API from "../API"
 
-const useUserStore = create((set)=>({
+const useUserStore = create((set,get)=>({
     loading:false,
     error:null,
     success:false,
     user:{},
+    notifications:[],
+    users:[],
+    actionloading:0,
 
     fetchUser: async()=>{
         set({loading:true})
           try {
               const { data } = await API.get("auth/me");
-            set({loading:false, user:data})
-            
+            set({loading:false, user:data, notifications: data.notifications})
+            // console.log(data)
+          } catch (error) {
+            console.log(error.message)
+            set({error: error.response?.data?.error, loading:false})
+          }
+    },
+    fetchUsers: async()=>{
+        set({loading:true})
+          try {
+              const { data } = await API.get("users/");
+              set({loading:false,users:data})
+    
           } catch (error) {
             console.log(error.message)
             set({error: error.response?.data?.error, loading:false})
@@ -40,6 +54,31 @@ const useUserStore = create((set)=>({
     signout:()=>{
         localStorage.removeItem("storyhub_token")
         window.location.href="/login"
+    },
+
+    followUser:async(userId)=>{
+        set({actionloading:userId})
+          try {
+              await API.post(`follow/${userId}`);
+              const updated =  get().users.map((user)=>user.id === userId? {...user, isFollowing:true, followersCount: parseInt(user.followersCount || 0)+1}:user)
+            set({users:updated, actionloading:0})
+               
+          } catch (error) {
+            console.log(error.message)
+            set({error: error.response?.data?.error, actionloading:0})
+          }
+    },
+    unFollowUser:async(userId)=>{
+        set({actionloading:userId})
+          try {
+              await API.delete(`follow/${userId}`);
+              const updated =  get().users.map((user)=>user.id === userId? {...user, isFollowing:false, followersCount: parseInt(user.followersCount || 0)-1}:user)
+            set({users:updated, actionloading:0})
+               
+          } catch (error) {
+            console.log(error.message)
+            set({error: error.response?.data?.error, actionloading:0})
+          }
     }
 }))
 
