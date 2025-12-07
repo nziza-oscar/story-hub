1a8179751efa34854788b57be0d3c06b2d30f815
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2025-12-07 19:46:00 +0200

    feat: client/src/store/useNotificationStore.js

--- client/src/store/useNotificationStore.js
+++ client/src/store/useNotificationStore.js
@@ -1,24 +1,37 @@
 import { create} from "zustand"
 import API from "../API"
 
-const useNotificationStore = create((set)=>({
+const useNotificationStore = create((set,get)=>({
     loading:false,
     error:null,
     success:false,
-    notificatios:[],
+    notifications:[],
     meta:{},
     fetchNotification: async()=>{
         set({loading:true})
           try {
               const { data } = await API.get("notifications/");
-              set({loading:false, notificatios: data.data, meta:data.meta})
-             
+              set({loading:false, notifications: data.data, meta:data.meta})
           } catch (error) {
             console.log(error.message)
             set({error: error.response?.data?.error, loading:false})
           }
     },
-   
+   markAsRead:async()=>{
+        set({loading:true})
+          try {
+              await API.patch("notifications/user/read-all");
+               const updated = get().notifications.map((n)=>({
+                ...n,
+                is_read:true
+               }))
+              set({loading:false, notifications: updated})
+              
+          } catch (error) {
+            console.log(error.message)
+            set({error: error.response?.data?.error, loading:false})
+          }
+    },
    
 }))
 
