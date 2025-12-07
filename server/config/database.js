3f3704c964bf3d7cc37a2e228fbf7e233b9fea93
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2025-12-07 19:46:05 +0200

    feat: server/config/database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,26 +1,26 @@
 require("dotenv").config()
 const { Sequelize} = require("sequelize")
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL,{
-//     dialect:"postgres",
-//     dialectOptions:{
-//         ssl:{require:true}
-//     },
-//     logging:false
-// });
-
-
-
-const sequelize = new Sequelize(
-    process.env.SQL_DATABASE,
-    process.env.SQL_USERNAME,
-    process.env.SQL_PASSWORD,
-    
-    {
-    dialect:"mysql",
-    host: process.env.SQL_HOST,
-    port: process.env.SQL_PORT,
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    dialect:"postgres",
+    dialectOptions:{
+        ssl:{require:true}
+    },
     logging:false
 });
+
+
+
+// const sequelize = new Sequelize(
+//     process.env.SQL_DATABASE,
+//     process.env.SQL_USERNAME,
+//     process.env.SQL_PASSWORD,
+    
+//     {
+//     dialect:"mysql",
+//     host: process.env.SQL_HOST,
+//     port: process.env.SQL_PORT,
+//     logging:false
+// });
 
 module.exports=sequelize
